652b2f32c2e0160a698105ee67cdae572945d710
lucas <lucassebah@gmail.com>
2025-10-24 19:47:08 -0300

    Agrego las clases del modelo con anotaciones correspondientes

--- src/main/java/model/paquete/PaqueteRefrigerado.java
+++ src/main/java/model/paquete/PaqueteRefrigerado.java
@@ -1,8 +1,28 @@
 package model.paquete;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+@PrimaryKeyJoinColumn(referencedColumnName = "id")
+
 public class PaqueteRefrigerado extends Paquete {
+    @PositiveOrZero
+    @Column(nullable = false)
     private Double temperaturaObjetivo;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private Double rangoMinimo;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private Double rangoMaximo;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private Integer maxHsFueraFrio;
 }
