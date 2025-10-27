97f795aff05e3cf3a0c27b19767758db6c9646c4
Emm4nue1 <ema.mendoza97@outlook.com>
2025-10-27 14:50:02 -0300

    Merge Service-Controller: sobrescribir conflictos con sus cambios

---
+++ src/main/java/demo/controller/PaqueteController.java
@@ -0,0 +1,8 @@
+package demo.controller;import java.util.Scanner;
+
+public class PaqueteController {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        leer.close();
+    }
+}
