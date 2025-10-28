0e6c0c90d3b6f151ac3de147e8e8595b6e695ba4
lucas <lucassebah@gmail.com>
2025-10-28 16:06:37 -0300

    Realizacion de PruebasUnitariasEnvio

--- src/test/java/service/EnvioServiceTestCase.java
+++ src/test/java/service/EnvioServiceTestCase.java
@@ -7,10 +7,15 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = DemoApplication.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -24,34 +29,57 @@ public class EnvioServiceTestCase {
     private PaqueteService paqueteService;
 
     PaqueteDTO paqueteDTO1;
-    PaqueteDTO paqueteDTO2;
+    EnvioDTO envioDtoAux;
     EnvioDTO envioDTO1;
+    List<PaqueteDTO> paquetesDto;
+
     @BeforeEach
     public void setUp() {
         paqueteDTO1= PaqueteDTO.builder().codigo("H123").peso(50).volumen(30).
                 nivelFragilidad("BAJA").seguroAdicional(true).tipo("PF").build();
-        paqueteService.crearPaquete(paqueteDTO1);
-        envioDTO1.builder().remitente("persona1").destinatario("persona2").direccionEntrega("palpala")
-                .estadoEnvio("GENERADO").comprobanteEntrega(true).paquetes(paqueteService.listarPaquetes()).build();
+        paquetesDto = new ArrayList<>();
+        paquetesDto.add(paqueteDTO1);
+        envioDTO1 = EnvioDTO.builder().remitente("persona1").destinatario("persona2").direccionEntrega("palpala")
+                .estadoEnvio("GENERADO").comprobanteEntrega(true).paquetes(paquetesDto).build();
+        envioDtoAux= null;
     }
     @Test
     public void testCrearEnvio(){
-
+        envioDtoAux =envioService.crearEnvio(envioDTO1);
+        assertNotNull(envioDtoAux);
     }
     @Test
     public void testListarEnvios(){
 
+        envioDtoAux = EnvioDTO.builder().remitente("persona3").destinatario("persona4").direccionEntrega("canal de Beagle")
+                .estadoEnvio("DEVUELTO").comprobanteEntrega(true).paquetes(paquetesDto).build();
+        envioService.crearEnvio(envioDTO1);
+        envioService.crearEnvio(envioDtoAux);
+        assertEquals(2,envioService.listarEnvio().size());
+
     }
     @Test
     public void testListarEnviosPorRemitente(){
-
+        envioDtoAux = EnvioDTO.builder().remitente("persona1").destinatario("persona4").direccionEntrega("canal de Beagle")
+                .estadoEnvio("ENTREGADO").comprobanteEntrega(true).paquetes(paquetesDto).build();
+        envioService.crearEnvio(envioDTO1);
+        envioService.crearEnvio(envioDtoAux);
+        assertEquals(2,envioService.listarPorRemitente("persona1").size());
     }
     @Test
     public void testListarEnviosPorDestinatario(){
-
+        envioDtoAux = EnvioDTO.builder().remitente("persona3").destinatario("persona2").direccionEntrega("gorriti")
+                .estadoEnvio("CANCELADO").comprobanteEntrega(true).paquetes(paquetesDto).build();
+        envioService.crearEnvio(envioDTO1);
+        envioService.crearEnvio(envioDtoAux);
+        assertEquals(2,envioService.listarPorDestinatario("persona2").size());
     }
     @Test
     public void testListarEnviosPorEstado(){
-
+        envioDtoAux = EnvioDTO.builder().remitente("persona3").destinatario("persona4").direccionEntrega("nieva")
+                .estadoEnvio("GENERADO").comprobanteEntrega(true).paquetes(paquetesDto).build();
+        envioService.crearEnvio(envioDTO1);
+        envioService.crearEnvio(envioDtoAux);
+        assertEquals(2,envioService.listarPorEstado("GENERADO").size());
     }
 }
